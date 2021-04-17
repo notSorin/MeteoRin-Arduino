98b543c66f6af87189b43225232254863257bb12
Sorin <sorindra@ucm.es>
2021-04-17 21:40:35 +0200

    Comments
    Code formatting.
    Changed how data is sent through bluetooth.

--- secondary/secondary.ino
+++ secondary/secondary.ino
@@ -1,43 +1,36 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
+constexpr int I2C_ADDRESS = 9;
+constexpr int RX_PIN = 2;
+constexpr int TX_PIN = 3;
+
+SoftwareSerial HC06(RX_PIN, TX_PIN);
+
 int temperature = 0;
 int humidity = 0;
 
-#define rxPin 2
-#define txPin 3
-
-SoftwareSerial HC06(rxPin, txPin);
-int BluetoothData;
-
 void setup()
 {
-  Serial.begin(9600);
-
   HC06.begin(9600);
-  
-  // Start the I2C Bus as Slave on address 9
-  Wire.begin(9); 
-  
-  // Attach a function to trigger when something is received.
-  Wire.onReceive(receiveEvent);
+  Wire.begin(I2C_ADDRESS); //Start the I2C Bus as Slave on address 9.
+  Wire.onReceive(receiveEvent); //Attach a function to trigger when something is received.
 }
 
+//Called whenever data is received throudh I2C.
 void receiveEvent(int bytes)
 {
-  temperature = Wire.read(); //Read one character from the I2C.
+  //Read the temperature first, then the humidity;
+  temperature = Wire.read();
   humidity = Wire.read();
-  
-  Serial.print("Received: t: ");
-  Serial.print(temperature);
-  Serial.print(" h: ");
-  Serial.println(humidity);
 
-  String s = String(temperature) + "x" + String(humidity);
+  //Create a string with the data, and send it through bluetooth.
+  String toSend = String(temperature) + " " + String(humidity);
   
-  HC06.println(s);
+  HC06.println(toSend);
 }
 
 void loop()
 {
+  //The secondary board does nothing in the main loop.
 }
